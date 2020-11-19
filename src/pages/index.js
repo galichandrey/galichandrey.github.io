c0286bb51d069fe5068c59787b848daef284699d
galichandrey <galich.andrey@gmail.com>
2020-11-19 22:56:10 +0700

    Changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import theme from "theme";
 import { Theme, Link, Text, Box } from "@quarkly/widgets";
 import { Helmet } from "react-helmet";
-import { Override, StackItem, Stack, Section, SocialMedia } from "@quarkly/components";
+import { Section, Override, StackItem, Stack, SocialMedia } from "@quarkly/components";
 export default (() => {
 	return <Theme theme={theme}>
 		<Helmet>
@@ -24,72 +24,75 @@ export default (() => {
 				text-align="center"
 				width="900px"
 			>
-				«Школа-студия Галич»
+				Школа-студия Галич
 			</Text>
-			<Text
-				as="p"
-				color="--grey"
-				max-width="700px"
-				margin="0 auto"
-				text-align="center"
-				font="--lead"
-			>
-				Индивидуальное обучение без отрыва от производства в удобном для вас графике по направлениям: маникюр, гель-лак, педикюр, наращивание ногтей, наращивание ресниц, оформление и окраска бровей{"\n\n"}
-			</Text>
-			<Stack margin-top="40px">
-				<StackItem width="25%" lg-width="50%" sm-width="100%">
-					<Override slot="StackItemContent" flex-direction="column" />
-					<Box
-						height="0"
-						margin="0 0 20px 0"
-						padding-bottom="100%"
-						background="url(https://uploads.quarkly.io/5fb6745ab1c8af001eb5555e/images/DIf-LZypLVI.jpg?v=2020-11-19T15:01:09.913Z) 50% 0/cover no-repeat"
-						top={0}
+		</Section>
+		<Section padding="20px 0 24px 0" sm-padding="40px 0" font="--base" color="--dark">
+			<Stack>
+				<StackItem width="20%" lg-width="100%" lg-order="2">
+					<Override slot="StackItemContent" text-align="center" width="200px" />
+					<Stack>
+						<StackItem width="50%" sm-width="100%">
+							<Override
+								slot="StackItemContent"
+								background="--color-lightD1"
+								padding="20px"
+								flex-direction="column"
+								width="220px"
+								min-width="220px"
+								height="250px"
+							/>
+							<Box
+								width="150px"
+								height="150px"
+								margin="0 auto"
+								background="url(https://uploads.quarkly.io/5fb6745ab1c8af001eb5555e/images/DIf-LZypLVI.jpg?v=2020-11-19T15:01:09.913Z) 50% 0/cover no-repeat"
+								border-radius="50%"
+							/>
+							<Text as="h3" font="normal 500 24px/1.2 --fontFamily-sans" margin="20px 0 5px 0">
+								Марина Галич{"\n\n"}
+							</Text>
+							<Text color="--grey" margin="0">
+								Директор{"\n\n"}
+							</Text>
+						</StackItem>
+					</Stack>
+				</StackItem>
+				<StackItem width="80%" lg-width="100%">
+					<Override
+						slot="StackItemContent"
+						lg-margin-bottom="24px"
+						flex-direction="column"
+						min-width="100%"
+						width="100%"
 					/>
-					<Text color="--grey" margin="0">
-						Директор
-					</Text>
-					<Text as="h3" font="--headline3" margin="5px 0 20px 0">
-						Марина Галич
-					</Text>
-				</StackItem>
-				<StackItem width="25%" lg-width="50%" sm-width="100%" sm-margin-top="24px">
-					<Override slot="StackItemContent" flex-direction="column" />
-					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(https://images.unsplash.com/photo-1546672741-d327539d5f13?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80) 50% 0/cover no-repeat" />
-					<Text color="--grey" margin="0">
-						Developer
-					</Text>
-					<Text as="h3" font="--headline3" margin="5px 0 20px 0">
-						David R. Lema
-					</Text>
-				</StackItem>
-				<StackItem width="25%" lg-width="50%" sm-width="100%" sm-margin-top="24px">
-					<Override slot="StackItemContent" flex-direction="column" />
-					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80) 50% 0/cover no-repeat" />
-					<Text color="--grey" margin="0">
-						Marketing
-					</Text>
-					<Text as="h3" font="--headline3" margin="5px 0 20px 0">
-						Janet E. Morey
-					</Text>
-				</StackItem>
-				<StackItem width="25%" lg-width="50%" sm-width="100%" sm-margin-top="24px">
-					<Override slot="StackItemContent" flex-direction="column" />
-					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(https://images.unsplash.com/photo-1510227272981-87123e259b17?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=666&q=80) 50% 0/cover no-repeat" />
-					<Text color="--grey" margin="0">
-						Designer
+					<Text
+						color="--grey"
+						letter-spacing="1px"
+						text-transform="uppercase"
+						margin="0 0 10px 0"
+						width="100%"
+						min-width="100%"
+						max-width="100%"
+					/>
+					<Text
+						as="h2"
+						font="normal 700 30px/1.2 --fontFamily-sans"
+						md-font="--headline3"
+						margin="0 0 0 10px"
+						width="100%"
+						min-width="100%"
+					>
+						«Индивидуальное и групповое обучение маникюру, гель-лаку, педикюру, наращиванию ногтей, наращиванию ресниц, оформлению и окраске бровей.
 						<br />
-					</Text>
-					<Text as="h3" font="--headline3" margin="5px 0 20px 0">
-						Claire R. Peery
-						<br />
+						Без отрыва от производства в удобном для вас графике - день и время выбираете вы»
 					</Text>
 				</StackItem>
 			</Stack>
+			<SocialMedia facebook="https://www.facebook.com/galichschool.studio" instagram="https://www.instagram.com/galichschool.studio" whatsapp="https://api.whatsapp.com/send?phone=79069543999" margin="0px 0px 10px 0px">
+				<Override slot="link" />
+			</SocialMedia>
 		</Section>
-		<SocialMedia facebook="https://www.facebook.com/galichschool.studio" instagram="https://www.instagram.com/galichschool.studio" whatsapp="https://api.whatsapp.com/send?phone=79069543999" margin="0px 0px 20px 0px">
-			<Override slot="link" />
-		</SocialMedia>
 		<Section background-color="--dark" text-align="center" padding="32px 0">
 			<Link
 				href="mailto:hello@company.com"
